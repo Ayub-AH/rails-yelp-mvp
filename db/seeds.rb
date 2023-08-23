c05115f1d1195c97f693e8c08944ac67861d6bcf
Ayub Hassan <ayyubahmed555@gmail.com>
2023-08-23 14:06:59 +0100

    rails new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+restaurants_data = [
+    { name: "Delicious Diner", address: "123 Main St", category: "american" },
+    { name: "Pasta Paradise", address: "456 Pasta Ave", category: "italian" },
+    { name: "Sushi Sensation", address: "789 Sushi Blvd", category: "japanese" },
+    { name: "French Bistro", address: "101 French Lane", category: "french" },
+    { name: "Belgian Waffles", address: "222 Waffle Rd", category: "belgian" }
+  ]
+  
+  restaurants_data.each do |restaurant_data|
+    Restaurant.create!(restaurant_data)
+  end
